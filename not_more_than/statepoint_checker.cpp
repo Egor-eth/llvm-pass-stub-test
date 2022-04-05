ee98444bbc560d966044543827d1cf144d093d40
Egor-eth <egor.prih@yandex.ru>
2022-04-05 22:29:33 +0300

    Second task

---
+++ not_more_than/statepoint_checker.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/InlineAsm.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+using namespace llvm;
+
+namespace {
+
+enum {MAX_NUM = 2};
+
+struct StatePointChecker : public FunctionPass {
+  static char ID;
+  StatePointChecker() : FunctionPass(ID) {}
+
+  bool runOnFunction(Function &F) override {
+
+    for(const BasicBlock &blk : F) {
+      for(const Instruction &ins : blk) {
+        if(ins.getOpcode() == Instruction::Call) {
+          const CallInst *callInst = cast<CallInst>(&ins);
+          if(callInst->getCalledFunction()->isIntrinsic()) {
+            auto optionalOpBundle = callInst->getOperandBundle("gc-live");
+            if(optionalOpBundle.hasValue()) {
+              const size_t ptrOps = std::count_if(optionalOpBundle->Inputs.begin(),
+                optionalOpBundle->Inputs.end(), [](const Use &use) {
+                return use->getType()->isPointerTy();
+              });
+              if(ptrOps > MAX_NUM) {
+                errs() 
+                  << "[Statepoint's checker] "
+                  << "Warning: maximum number of statepoint's operands is more than " 
+                  << MAX_NUM << ": " << ins << "\n";
+              }
+            }
+          }
+        }
+      }
+    }
+    return false;
+  }
+}; // end of struct StatePointChecker
+}  // end of anonymous namespace
+
+char StatePointChecker::ID = 0;
+static RegisterPass<StatePointChecker> X("statepoint_checker", "Statepoint's checker Pass",
+                                   false /* Only looks at CFG */,
+                                   false /* Analysis Pass */);
+
+static RegisterStandardPasses Y(
+    PassManagerBuilder::EP_EarlyAsPossible,
+    [](const PassManagerBuilder &Builder,
+       legacy::PassManagerBase &PM) { PM.add(new StatePointChecker()); });
